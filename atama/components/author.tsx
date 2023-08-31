88e6ca6c63e7251908c4a6b8f0b57f6d8c24171d
Sammy Braun <sammy@atama.co>
2023-08-31 14:26:47 +0200

    Added styles to author component

--- atama/components/author.tsx
+++ atama/components/author.tsx
@@ -1,3 +1,5 @@
+import styles from "./author.module.css";
+
 export function Author({
   name,
   bio,
@@ -8,8 +10,8 @@ export function Author({
   atama: object;
 }) {
   return (
-    <div {...atama}>
-      <strong>{name}</strong>
+    <div {...atama} className={styles.author}>
+      <h3 className={styles.heading}>{name}</h3>
       <p>{bio}</p>
     </div>
   );
